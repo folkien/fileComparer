981ed3edfe184cb68a2bd51f6c03ad7dbbaf6eaf
Slawomir Paszko <folkus@gmail.com>
2014-09-30 22:53:54 +0200

    Wersja 0.1 programu. Coś działa.

--- program1.py
+++ program1.py
@@ -1,19 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #Otwiermy plik o argumencie 1
-fin_lines = [];
+lines1 = [];
 fin = open('file1.txt', 'r')
 for line in fin:
-    fin_lines[len(fin_lines):] = line;
+    lines1[len(lines1):] = [line];
 
-print fin_lines
+print lines1
 fin.close()
 
 #Otwiermy plik o argumencie 2
-fin_lines2 = [];
+lines2 = [];
 fin2 = open('file2.txt', 'r')
 for line in fin2:
-    fin_lines2[len(fin_lines2):] = line;
+    lines2[len(lines2):] = [line];
 
-print fin_lines2
+print lines2
 fin2.close()
 
-        
+
+#Tylko rozniace sie linijki
+for line in lines1:
+    if line in lines2 :
+        # Usuwamy elementy z kazdej listy
+        lines1.pop( lines1.index(line))
+        lines2.pop( lines2.index(line))
+
+# Printowanie na ekranie listy numer 1.
+print u"Tylko różniące się linijki :"
+for line in lines1:
+    print line
